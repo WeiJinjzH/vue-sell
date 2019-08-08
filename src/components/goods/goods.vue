d47365ee45e646ff721083e73f9c86e64ecb1f74
WeiJinjzH <991775996@qq.com>
2019-08-08 15:27:16 +0800

    better-scroll的运用以及左右联动

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="goods">
-        <div class="menu-wrapper">
+        <div class="menu-wrapper" v-el:menu-wrapper>
             <ul>
-                <li v-for="item in goods" :key="item" class="menu-item">
+                <li v-for="item in goods" :key="item" class="menu-item" :class="{'current': currentIndex === $index}" @click="selectMenu($index, $event)">
                     <span class="text border-1px">
                         <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
                     </span>
                 </li>
             </ul>
         </div>
-        <div class="foods-wrapper">
+        <div class="foods-wrapper" v-el:foods-wrapper>
             <ul>
-                <li v-for="item in goods" :key="item" class="food-list">
+                <li v-for="item in goods" :key="item" class="food-list food-list-hook">
                     <h1 class="title">{{item.name}}</h1>
                     <ul>
                         <li v-for="food in item.foods" :key="food" class="food-item border-1px">
@@ -22,12 +22,10 @@
                                 <h2 class="name">{{food.name}}</h2>
                                 <p class="desc">{{food.description}}</p>
                                 <div class="extra">
-                                    <span class="count">月售{{food.sellCount}}份</span>
-                                    <span>好评率{{food.rating}}%</span>
+                                    <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                                 </div>
                                 <div class="price">
-                                    <span class="now">¥{{food.price}}</span>
-                                    <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                                    <span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
                                 </div>
                             </div>
                         </li>
@@ -38,6 +36,8 @@
     </div>
 </template>
 <script type="text/ecmascrpt-6">
+    import BScroll from 'better-scroll'
+
     const ERR_OK = 0
 
     export default {
@@ -49,6 +49,20 @@
         data() {
             return {
                 goods: [],
+                listHeight: [],
+                scrollY: 0,
+            }
+        },
+        computed: {
+            currentIndex() {
+                for(let i = 0; i < this.listHeight.length; i++) {
+                    let height1 = this.listHeight[i]
+                    let height2 = this.listHeight[i + 1]
+                    if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+                        return i
+                    }
+                }
+                return 0
             }
         },
         created() {
@@ -57,8 +71,43 @@
                 res = res.body
                 if (res.errno === ERR_OK) {
                     this.goods = res.data
+                    this.$nextTick(() => {
+                        this._initScroll()
+                        this._calculateHeight()
+                    })
                 }
             })
+        },
+        methods: {
+            selectMenu(index, event) {
+                if (!event._constructed) {
+                    return
+                }
+                let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+                let el = foodList[index]
+                this.foodsScroll.scrollToElement(el, 300)
+            },
+            _initScroll() {
+                this.menuScroll = new BScroll(this.$els.menuWrapper, {
+                    click: true
+                })
+
+                this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+                    probeType: 3
+                })
+                this.foodsScroll.on('scroll', (pos) => {
+                    this.scrollY = Math.abs(Math.round(pos.y))
+                })
+            },
+            _calculateHeight() {
+                let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+                let height = 0
+                this.listHeight.push(height)
+                for(let i = 0; i < foodList.length; i++) {
+                    height += foodList[i].clientHeight
+                    this.listHeight.push(height)
+                }
+            }
         },
     }
 </script>
@@ -83,6 +132,14 @@
                 height: 54px
                 width: 56px
                 line-height: 14px
+                &.current
+                    position: relative
+                    z-index: 10
+                    margin-top: -1px
+                    background: #ffffff
+                    font-weight: 400
+                    .text
+                        border-none()
                 .icon
                     display: inline-block
                     vertical-align: top
@@ -138,6 +195,7 @@
                         color: rgb(7, 17, 27)
                     .desc
                         margin-bottom: 8px
+                        line-height: 12px
                     .extra, .desc
                         line-height: 10px
                         font-size: 10px
