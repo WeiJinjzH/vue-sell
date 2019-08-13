48d94a871f14598c7d409d52d305bbe75ecc5986
WeiJinjzH <991775996@qq.com>
2019-08-13 09:55:43 +0800

    购物车组件 根据计算属性来设置class的变化

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,16 +3,19 @@
         <div class="content">
             <div class="content-left">
                 <div class="logo-wrapper">
-                    <div class="logo">
-                        <span class="icon-shopping_cart"></span>
+                    <div class="logo" :class="{'highlight': totalCount > 0}">
+                        <span class="icon-shopping_cart" :class="{'highlight': totalCount > 0}"></span>
+                    </div>
+                    <div class="num" v-show="totalCount > 0">
+                        {{totalCount}}
                     </div>
                 </div>
-                <div class="price">¥{{minPrice}}</div>
+                <div class="price" :class="{'highlight': totalPrice > 0}">¥{{totalPrice}}</div>
                 <div class="desc">另需配送费¥{{deliveryPrice}}元</div>
             </div>
             <div class="content-right">
-                <div class="pay">
-                    ¥{{minPrice}}元起送
+                <div class="pay" :class="payClass">
+                    {{payDesc}}
                 </div>
             </div>
         </div>
@@ -21,6 +24,21 @@
 <script type="text/ecmascrpt-6">
     export default {
         props: {
+            selectFoods: {
+                type: Array,
+                default() {
+                    return [
+                        {
+                            price: 10,
+                            count: 1,
+                        },
+                        {
+                            price: 20,
+                            count: 2,
+                        }
+                    ]
+                },
+            },
             deliveryPrice: {
                 type: Number,
                 default: 0,
@@ -29,7 +47,40 @@
                 type: Number,
                 default: 0,
             }
-        }
+        },
+        computed: {
+            totalPrice() {
+                let total = 0
+                this.selectFoods.forEach((food) => {
+                    total += food.price * food.count
+                })
+                return total
+            },
+            totalCount() {
+                let count = 0
+                this.selectFoods.forEach((food) => {
+                    count += food.count
+                })
+                return count
+            },
+            payDesc() {
+                if (this.totalPrice === 0) {
+                    return `¥${this.minPrice}元起送`
+                } else if (this.totalPrice < this.minPrice) {
+                    let diff = this.minPrice - this.totalPrice
+                    return `还差¥${diff}元起送`
+                } else {
+                    return '去结算'
+                }
+            },
+            payClass() {
+                if (this.totalPrice < this.minPrice) {
+                    return 'not-enough'
+                } else {
+                    return 'enough'
+                }
+            }
+        },
     }
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
@@ -64,10 +115,28 @@
                         border-radius: 50%
                         background: #2b343c
                         text-align: center
+                        &.highlight
+                            background: rgb(0, 160, 220)
                         .icon-shopping_cart
                             line-height: 44px
                             font-size: 24px
                             color: #80858a
+                            &.highlight
+                                color: #ffffff
+                    .num
+                        position: absolute
+                        top: 0
+                        right: 0
+                        width: 24px
+                        height: 16px
+                        line-height: 16px
+                        text-align: center
+                        border-radius: 16px
+                        font-size: 9px
+                        font-weight: 700
+                        color: #ffffff
+                        background: rgb(240, 20, 20)
+                        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
                 .price
                     display: inline-block
                     vertical-align: top
@@ -78,6 +147,8 @@
                     border-right: 1px solid rgba(255, 255, 255, 0.1)
                     font-size: 16px
                     font-weight: 700
+                    &.highlight
+                        color: #ffffff
                 .desc
                     display: inline-block
                     margin: 12px 0 0 12px
@@ -93,5 +164,9 @@
                     text-align: center
                     font-size: 12px
                     font-weight: 700
-                    background: #2b333b
+                    &.not-enough
+                        background: #2b333b
+                    &.enough
+                        background: #00b43c
+                        color: #ffffff
 </style>
